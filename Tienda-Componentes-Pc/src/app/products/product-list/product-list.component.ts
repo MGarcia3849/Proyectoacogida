ec781c6883debda00b9f19ef47daae57e7a297e1
Hugo <hob0001@alu.medac.es>
2025-04-07 14:33:55 +0200

    Se crea la lista de Poductos

--- Tienda-Componentes-Pc/src/app/products/product-list/product-list.component.ts
+++ Tienda-Componentes-Pc/src/app/products/product-list/product-list.component.ts
@@ -9,7 +9,8 @@ import { ProductosService, Producto } from '../../core/services/productos.servic
   standalone: true,
   selector: 'app-product-list',
   imports: [CommonModule, RouterModule],
-  templateUrl: './product-list.component.html'
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent {
   private productosService = inject(ProductosService);
