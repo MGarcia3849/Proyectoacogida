669f9551a3a04d7e07effa3c9458d3c1819b4245
Hugo <hob0001@alu.medac.es>
2025-04-07 14:53:43 +0200

    Arreglado el listado de productos + el boton Volver a tienda

--- Tienda-Componentes-Pc/src/app/app.routes.ts
+++ Tienda-Componentes-Pc/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { MainLayoutComponent } from './core/layout/main-layout/main-layout.component';
+import { ProductDetailComponent } from './products/product-detail/product-detail.component';
 
 export const appRoutes: Routes = [
   {
@@ -16,6 +17,7 @@ export const appRoutes: Routes = [
         loadChildren: () =>
           import('./products/products.routes').then((m) => m.PRODUCTS_ROUTES)
       },
+      { path: 'producto/:id', component: ProductDetailComponent },
       {
         path: 'carrito',
         loadComponent: () =>
